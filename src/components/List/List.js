b1a1c419658bd57ad441972458e1db48b7d93f05
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-11-02 00:36:53 +0100

    Add columns' titles_ 15.4

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import Column from '../Column/Column';
+import PropTypes from 'prop-types';
 
+//kompenent klasowy
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    image: PropTypes.string,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
         <section className={styles.component}>
-          <Hero />
+          <Hero titleText={this.props.title} titleImage={this.props.titleImage}/>
+          <div className={styles.description}>
+              {this.props.children}
+          </div>
+          <div className={styles.columns}>
+          <Column columnTitle='Animals'/>
+          <Column columnTitle='Plants'/>
+          <Column columnTitle='Minerals'/>
+          </div>
         </section>
     )
   }
