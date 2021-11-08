e06f817cb1badee4ddbf744906d1136d8eb99191
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-11-08 14:19:49 +0100

    Redux transfer Cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-//import Card from '../Card/Card';
+import Card from '../Card/Card';
 //import Creator from '../Creator/Creator';
 //import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon';
@@ -18,21 +18,22 @@ class Column extends React.Component {
 
   render() {
 
-    const {title, icon} = this.props;
+    const {title, icon, cards} = this.props;
     
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{title}
           <span className={styles.icon}><Icon name={icon} /></span>
         </h3>
-        {/*<div className={styles.cards}>
-          {this.state.cards.map(({key, ...cardProps}) => (
-            <Card key={key} {...cardProps} />
-          ))} 
+        <div className={styles.cards}>
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
+          ))}
         </div>
+        {/*
         <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-          </div>*/}
+        </div>*/}
       </section>
     );
   }
