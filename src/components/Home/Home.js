3e3926510b61d8c68f0faf38872dc2a76ce7281c
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-11-15 23:10:27 +0100

    ex.17.2

---
+++ src/components/Home/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import List from '../List/ListContainer';
+import styles from './Home.scss';
+import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer';
+
+class Home extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
+    lists: PropTypes.array,
+  }
+
+  render() {
+    const {title, subtitle, lists} = this.props;
+    return (
+      <main className={styles.component}>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        {lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))}
+        <Search />
+      </main>
+    );
+  }
+}
+
+export default Home;
